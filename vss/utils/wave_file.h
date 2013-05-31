882457b664fd177ba3a866824a5b7534da7ec77e
quangpl00809 <quangpl00809@fpt.edu.vn>
2013-05-31 17:28:05 +0000

    add class wave_file

---
+++ vss/utils/wave_file.h
@@ -0,0 +1,42 @@
+/*
+ * wave_file.h
+ *
+ *  Created on: May 31, 2013
+ *      Author: quangpham
+ */
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+#ifndef WAVE_FILE_H_
+#define WAVE_FILE_H_
+
+class WaveFile {
+public:
+    WaveFile();
+    virtual ~WaveFile();
+
+    void init();
+    void update_size();
+    void set_data(short data[], int n);
+    void add_data(vector<short> more_data);
+    vector<short> get_data(int from, int to);
+
+    char chunk_id[4];
+    int chunk_size;
+    char format[4];
+    char subchunk1_id[4];
+    int subchunk1_size;
+    short compression_code;
+    short number_channels;
+    int sample_rate;
+    int byte_rate;
+    short block_align;
+    short bits_per_sample;
+    char subchunk2_id[4];
+    int subchunk2_size;
+    vector<short> data;
+};
+
+#endif /* WAVE_FILE_H_ */
