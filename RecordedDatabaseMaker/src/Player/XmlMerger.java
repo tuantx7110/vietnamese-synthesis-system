f89cc63d050a47e16840d891063b4429e28e0c3b
quangpl00809 <quangpl00809@fpt.edu.vn>
2013-07-13 01:15:30 +0000

    add compound dict generator

--- RecordedDatabaseMaker/src/Player/XmlMerger.java
+++ RecordedDatabaseMaker/src/Player/XmlMerger.java
@@ -10,11 +10,13 @@ public class XmlMerger {
     private File[] xmlFiles;
     private File outputFile;
     private File statisticFile;
+    private File dictionaryFile;
 
     public XmlMerger(File[] xmlFiles, String outputFolder) {
         this.xmlFiles = xmlFiles;
         this.outputFile = new File(outputFolder + "/recorded_database_description.xml");
         this.statisticFile = new File(outputFolder + "/statistic.txt");
+        this.dictionaryFile = new File(outputFolder + "/compound_dictionary.txt");
     }
 
     public String start() throws Exception {
@@ -31,10 +33,14 @@ public class XmlMerger {
         }
 
         writer.close();
+
         statistic.writeStatisticFile(statisticFile);
+        statistic.writeCompoundDictionary(dictionaryFile);
+
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         PrintStream out = new PrintStream(baos);
         statistic.display(out, false);
+
         return baos.toString();
     }
 }
