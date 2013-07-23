fb4c170d8e5cba3d9a0a258cdc1167ba8a907d99
quangpl00809 <quangpl00809@fpt.edu.vn>
2013-07-23 05:26:13 +0000

    Add statistic for recorded database

--- vss/searcher/binary_database_converter.h
+++ vss/searcher/binary_database_converter.h
@@ -8,9 +8,14 @@
 #ifndef BINARY_DATABASE_CONVERTER_H
 #define	BINARY_DATABASE_CONVERTER_H
 
+#include <set>
+using namespace std;
+
 #include "xml_database_reader.h"
 #include "../encoding/binary_file_writer.h"
 #include "../utils/constants.h"
+#include "../utils/text_file_utils.h"
+#include "../utils/string_utils.h"
 
 class BinaryDatabaseConverter {
 public:
@@ -22,6 +27,7 @@ public:
 private:
     XmlDatabaseReader database_reader;
     BinaryFileWriter database_writer;
+    void save_distinct_syllables(set<string> syllables, string filename);
 };
 
 #endif	/* BINARY_DATABASE_CONVERTER_H */
--- vss/utils/constants.h
+++ vss/utils/constants.h
@@ -29,6 +29,7 @@ const string kDictionaryPath = "parser/dictionary/";
 const string kRecordedDatabasePath = "searcher/recorded_database/";
 const string kXmlRecordedDatabasePath = kRecordedDatabasePath + "recorded_database_description.xml";
 const string kBinaryRecordedDatabasePath = kRecordedDatabasePath + "recorded_database_description.bin";
+const string kDatabaseStatisticPath = kRecordedDatabasePath + "statistic.txt";
 
 #endif	/* CONSTANTS_H */
 
