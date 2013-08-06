09ab2089821ede44b4d6610ee04f4bce3e86a900
hieult01060@fpt.edu.vn <hieult01060@fpt.edu.vn@b7735990-2d5d-ca89-ba81-b592e1f1d15c>
2013-08-06 20:04:13 +0000

    test thành synthesis many

--- trunk/vss/test/test_synthesis_diphone.h
+++ trunk/vss/test/test_synthesis_diphone.h
@@ -5,8 +5,8 @@
 #include "../test/test_binary_encoding.h"
 #include "../searcher/binary_database_converter.h"
 #include "../test/test_database_reader.h"
-#include "../synthesis/syllable_synthesis.h"
-
+//#include "../synthesis/syllable_synthesis.h"
+#include "../synthesis/syllable_synthesis_many.h"
 using namespace std;
 
 void test_synthesis_diphone();
